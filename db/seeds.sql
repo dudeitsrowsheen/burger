bd6bb800bb9d5958e0a1c0d986d54e4bf2e79064
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-07-21 23:34:38 -0500

    file setup

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO "burgers" (burger_name, devoured) 
+VALUES ("cheeseburger", true);
+
+INSERT INTO "burgers" (burger_name, devoured) 
+VALUES ("Whooper", false);
+
+INSERT INTO "burgers" (burger_name, devoured) 
+VALUES ("Breakfast burger", true);
